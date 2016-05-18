67aaee59680d0408f7c7b156183636c4cba9bcd3
vikaba <vikaba@bergen.org>
2016-05-17 21:01:50 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
-var modal = document.getElementById("modal");
-var btn = document.getElementById("studentBtn");
-var span = document.getElementById("close");
+//var modal = document.getElementById("modal");
+//var btn = document.getElementById("studentBtn");
+//var span = document.getElementById("close");
 
-btn.onclick = function(){
-  modal.style.display = "block";
+//btn.onclick = function(){
+  //modal.style.display = "block";
   
-span.onclick = function(){
-  modal.style.display = "none";
+//span.onclick = function(){
+  //modal.style.display = "none";
   
-window.onclick = function(event) {
-  if (event.target == modal ) {
-    modal.style.display = "none";
+//window.onclick = function(event) {
+ // if (event.target == modal ) {
+    //modal.style.display = "none";
