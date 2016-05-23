426352547ad549b115610c32efd97f67d980805b
vikaba <vikaba@bergen.org>
2016-05-22 20:53:42 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,6 @@
-//var modal = document.getElementById("modal");
-//var btn = document.getElementById("studentBtn");
-//var span = document.getElementById("close");
-
-//btn.onclick = function(){
-  //modal.style.display = "block";
-  
-//span.onclick = function(){
-  //modal.style.display = "none";
-  
-//window.onclick = function(event) {
- // if (event.target == modal ) {
-    //modal.style.display = "none";
+$(document).ready(function() {
+  $('#student').click(function() {
+    $('#studentModal').fadeIn('slow');
+  });
+  });
+});
